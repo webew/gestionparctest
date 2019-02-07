bee934abc99b15547f7a4be96bd2dae76f68ae77
webew <fillion.bertrand@gmail.com>
2019-02-07 23:38:49 +0100

    appli

--- index.php
+++ index.php
@@ -5,16 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <nav>
-        <a href="liste_utilisateurs.php">Liste des utilisateurs</a> | 
-        <a href="liste_locaux.php">Liste des locaux</a> | 
-        <a href="liste_pcs.php">Liste des pcs</a> | 
-        <a href="liste_moniteurs.php">Liste des moniteurs</a> | 
-        <a href="liste_imprimantes.php">Liste des imprimantes</a> | 
-        <a href="generate_datas.php">Générer les données</a>
-    </nav>
-
+    <?php include 'menu.php' ?>
 </body>
 </html>
